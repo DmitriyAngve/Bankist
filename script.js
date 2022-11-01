4cf24365954661ce48ab170de065fd01e9bbbafb
Dmitriy Angve <kirirch129@gmail.com>
2022-11-01 15:24:14 +0300

    #

--- script.js
+++ script.js
@@ -85,3 +85,16 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+// Initials
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
